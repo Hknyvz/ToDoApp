a8932bf9e7862fa71367776d9325db569a9b844a
Hakan Yavuz <hknyvz_@hotmail.com>
2021-03-31 19:07:42 +0300

    Mimari oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react"
+import TodoList from './Components/TodoList/TodoList';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import CalendarPage from './Components/Calendar/CleandarPage';
+import SignUp from './Components/SignUp/SignUp';
+import SignIn from './Components/SignIn/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <TodoList></TodoList>
+          </Route>
+          <Route path="/signin" exact>
+            <SignIn></SignIn>
+          </Route>
+          <Route path="/signup" exact>
+            <SignUp></SignUp>
+          </Route>
+          <Route path="/calendar" exact>
+            <CalendarPage></CalendarPage>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
